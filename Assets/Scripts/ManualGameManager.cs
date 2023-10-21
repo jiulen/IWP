09fe416e56e65832b36ea934119eb9da0a6d5cb4
jiulen <wongjiulen.coding@gmail.com>
2023-10-22 02:31:29 +0800

    Setup 1

---
+++ Assets/Scripts/ManualGameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualGameManager : MonoBehaviour
+{
+    int currentFrame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Physics2D.Simulate(Time.fixedDeltaTime);
+        currentFrame++;
+    }
+}
