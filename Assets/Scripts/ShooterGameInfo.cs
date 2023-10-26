8452bcd669520569452fac2c73c9aa6b95b0d596
jiulen <wongjiulen.coding@gmail.com>
2023-10-26 22:19:17 +0800

    import player spritesheet

--- Assets/Scripts/ShooterGameInfo.cs
+++ Assets/Scripts/ShooterGameInfo.cs
@@ -2,12 +2,7 @@ using UnityEngine;
 
 public class ShooterGameInfo
 {
-    public const float ASTEROIDS_MIN_SPAWN_TIME = 5.0f;
-    public const float ASTEROIDS_MAX_SPAWN_TIME = 10.0f;
-
-    public const float PLAYER_RESPAWN_TIME = 4.0f;
-
-    public const int PLAYER_MAX_LIVES = 3;
+    public const int PLAYER_MAX_LIVES = 1;
 
     public const string PLAYER_LIVES = "PlayerLives";
     public const string PLAYER_READY = "IsPlayerReady";
--- Assets/Scripts/ShooterGameManager.cs
+++ Assets/Scripts/ShooterGameManager.cs
@@ -1,15 +1,202 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class ShooterGameManager : MonoBehaviour
+public class ShooterGameManager : MonoBehaviourPunCallbacks
 {
+    public static ShooterGameManager Instance = null;
+
+    public Text InfoText;
+
     int currentFrame;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Awake()
     {
-        
+        Instance = this;
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+
+        CountdownTimer.OnCountdownTimerHasExpired += OnCountdownTimerIsExpired;
+    }
+
+    public void Start()
+    {
+        Hashtable props = new Hashtable
+            {
+                {ShooterGameInfo.PLAYER_LOADED_LEVEL, true}
+            };
+        PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+
+        CountdownTimer.OnCountdownTimerHasExpired -= OnCountdownTimerIsExpired;
+    }
+
+    private IEnumerator EndOfGame(string winner, bool draw = false)
+    {
+        float timer = 2.0f;
+
+        while (timer > 0.0f)
+        {
+            if (!draw)
+            {
+                InfoText.text = string.Format("{0} wins", winner);
+            }
+            else
+            {
+                InfoText.text = "DRAW";
+            }
+
+            yield return new WaitForEndOfFrame();
+
+            timer -= Time.deltaTime;
+        }
+
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("LobbyScene");
+    }
+
+    public override void OnLeftRoom()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        if (PhotonNetwork.LocalPlayer.ActorNumber == newMasterClient.ActorNumber)
+        {
+
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        CheckEndOfGame();
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        if (changedProps.ContainsKey(ShooterGameInfo.PLAYER_LIVES))
+        {
+            CheckEndOfGame();
+            return;
+        }
+
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+
+        // if there was no countdown yet, the master client (this one) waits until everyone loaded the level and sets a timer start
+        int startTimestamp;
+        bool startTimeIsSet = CountdownTimer.TryGetStartTime(out startTimestamp);
+
+        if (changedProps.ContainsKey(ShooterGameInfo.PLAYER_LOADED_LEVEL))
+        {
+            if (CheckAllPlayerLoadedLevel())
+            {
+                if (!startTimeIsSet)
+                {
+                    CountdownTimer.SetStartTime();
+                }
+            }
+            else
+            {
+                // not all players loaded yet. wait:
+                Debug.Log("setting text waiting for players! ", this.InfoText);
+                InfoText.text = "Waiting for other player...";
+            }
+        }
+    }
+
+    // called by OnCountdownTimerIsExpired() when the timer ended
+    private void StartGame()
+    {
+        Debug.Log("StartGame!");
+
+        // on rejoin, we have to figure out if the spaceship exists or not
+        // if this is a rejoin (the ship is already network instantiated and will be setup via event) we don't need to call PN.Instantiate
+
+        //Spawn player
+        //PhotonNetwork.Instantiate("Spaceship", position, rotation, 0);      // avoid this call on rejoin (ship was network instantiated before)
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+        }
+    }
+
+    private bool CheckAllPlayerLoadedLevel()
+    {
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            object playerLoadedLevel;
+
+            if (p.CustomProperties.TryGetValue(ShooterGameInfo.PLAYER_LOADED_LEVEL, out playerLoadedLevel))
+            {
+                if ((bool)playerLoadedLevel)
+                {
+                    continue;
+                }
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private void CheckEndOfGame()
+    {
+        bool playerDead = false;
+        string winner = "";
+        bool draw = true;
+
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            object lives;
+            if (p.CustomProperties.TryGetValue(ShooterGameInfo.PLAYER_LIVES, out lives))
+            {
+                if ((int)lives <= 0)
+                {
+                    playerDead = true;
+                }
+                else
+                {
+                    winner = p.NickName;
+                    draw = false;
+                }
+            }
+        }
+
+        if (playerDead)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                StopAllCoroutines();
+            }
+
+            StartCoroutine(EndOfGame(winner, draw));
+        }
+    }
+
+    private void OnCountdownTimerIsExpired()
+    {
+        StartGame();
     }
 
     private void FixedUpdate()
