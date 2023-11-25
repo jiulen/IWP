8943c61843b5070ade1c51ea78cfff3c760b5b8f
jiulen <wongjiulen.coding@gmail.com>
2023-11-25 18:06:17 +0800

    block sprite animations

---
+++ Assets/Scripts/FrameBehaviours/Player/PlayerBlock.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlock: PlayerFrameBehaviour
+{
+    public bool goLeft = false;
+    public bool forwards = true;
+    public bool firstHalf = true; //check if walk cycle is first or second half
+
+    [SerializeField] float walkForce;
+    [SerializeField] string walkForwardAnim_1, walkForwardAnim_2, walkBackwordAnim_1, walkBackwordAnim_2;
+
+    public override void GoToFrame()
+    {
+        switch (frameNum)
+        {
+            case 0:
+                if (forwards)
+                {
+                    currentAnimName = firstHalf ? walkForwardAnim_1 : walkForwardAnim_2;
+                }
+                else
+                {
+                    currentAnimName = firstHalf ? walkBackwordAnim_1 : walkBackwordAnim_2;
+                }
+                AnimatorChangeAnimation(currentAnimName);
+
+                Vector2 walkDir = Vector2.right * (goLeft ? -1 : 1);
+                rb.AddForce(walkDir * walkForce, ForceMode2D.Impulse);
+                break;
+            case 17: //end
+                EndAnimation();
+
+                rb.velocity = Vector2.zero;
+                break;
+        }
+
+        AnimatorSetFrame();
+    }
+}
